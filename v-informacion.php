dc37c45059ab78dc64e5850770619919eea395ab
daniela-stack <81368621+daniela-stack@users.noreply.github.com>
2021-07-02 10:04:10 -0500

    corrección de ortografía

---
+++ v-informacion.php
@@ -0,0 +1,31 @@
+<!-- este archivo me muestra la informacion del traductor. -->
+
+<div class="hero-unit" >
+    <h1>Guia del traductor Native</h1>
+    <p>EL traductor se realizo con el fin de conservar el idioma tucano.</p>
+    
+</div>
+
+<div class="row">
+    <div class="span3">
+    
+        <h1 class="display-4">Por qué?</h1>
+         <p class="lead">El Tradurctor Native realiza traducciones de la lengua Tucano originaria de la Amazonia, hablada por la etnia Jepa majsa, a punto de desaparecer. </p>
+        
+    </div>
+    <div class="span3">
+        <h1 class="display-4">Qué traduce?</h1>
+        <p class="lead">Toda palabra o frase que desea aprender o conocer en la lengua Tucano. A continuación encontraras una lista de las palabras </p>
+        <p><a class="btn" href="otro-ejemplo.php">Lista de palabras</a></p> <!--me muestra el contenido del traductor en PDF (una lista) -->
+    </div>
+    <div class="span3">
+        <h1 class="display-4">Como funciona?</h1>
+        <p class="lead">Solo debe digitar la palabra o frase que desea traducir, elegir el idioma y como resultado mostrara la traducción, en el idioma que haya elegido, en este caso de tucano a español o de español a tucano.</p>
+       
+    </div>
+    <div class="span3">
+         <h1 class="display-4">Creadoras</h1>
+         <p class="lead">Aprendices del Tecnologo ADSI, Deimi Gomez, Lizbeth Caro, Daniela Avellaneda.</p>
+    
+    </div>
+</div>
